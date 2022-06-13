c684b04d7ca92e9d3af1f4fbe27a3d629e44daa8
kshitijbooya <98323378+kshitijbooya@users.noreply.github.com>
2022-06-13 17:18:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+    var mouseEvent = "empty";
+    var x,y
+
+    
+
+    canvas = document.getElementById('myCanvas');
+    ctx = canvas.getContext("2d");
+    
+    //Define two variables color and width_of_line and assign values.
+    color = "black";
+    width = 1;
+
+    canvas.addEventListener("mousedown", my_mousedown);
+
+    function my_mousedown(e)
+    {
+      
+        color = document.getElementById("color").value;
+        width = document.getElementById("width_of_line").value;
+
+        mouseEvent = "mouseDown";
+    }
+
+
+
+
+
+    canvas.addEventListener("mousemove", my_mousemove);
+    //Create the my_mouseup() function. Assign “mouseUP” in the variable mouseEvent.
+    function my_mousemove(e)
+    {
+        current_position_of_mouse_x = e.clientX - canvas.offsetLeft;
+        current_position_of_mouse_y = e.clientY - canvas.offsetTop;
+
+        if (mouseEvent =="mouseDown"){
+            ctx.beginPath();
+            ctx.strokeStyle = color;
+            ctx.lineWidth = width;
+            ctx.moveTo(x,y);
+            ctx.lineTo(current_position_of_mouse_x,current_position_of_mouse_y);
+            ctx.stroke();
+        }
+
+        x = current_position_of_mouse_x;
+        y = current_position_of_mouse_y;
+        
+    }
+
+function clearArea() {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+}
